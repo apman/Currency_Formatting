4d40dea4290d23acdf8a1a1f9f8bc449731412fd
apman <anna.malchowperryman@shopify.com>
2018-06-14 20:13:27 -0400

    Add a CurrencyScraper component that reads from currency-iso.org xml file

---
+++ src/Resources/CurrencyScraper.js
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+
+class CurrencyScraper extends Component {
+
+    constructor() {
+        super();
+        fetch('./list_one.xml')
+        .then(response => response.text())
+        .then(str => {
+            // console.log(str);
+            return (new DOMParser()).parseFromString(str, "text/xml")
+        })
+        .then(xmlDoc => {
+            const elements = xmlDoc.getElementsByTagName("CcyNtry");
+            let currencies = [];
+            for (let i = 0; i < elements.length; i++) {
+                if (elements[i].getElementsByTagName("Ccy")[0]) {
+                    const currencyCode = elements[i].getElementsByTagName("Ccy")[0].childNodes[0].nodeValue;
+                    const existingEntry = currencies.find(currency => currency.code === currencyCode);
+                    if (existingEntry) {
+                        existingEntry.countries.push(elements[i].getElementsByTagName("CtryNm")[0].innerHTML);
+                    } else {
+                        let currency = {
+                            code: currencyCode, 
+                            name: elements[i].getElementsByTagName("CcyNm")[0].innerHTML,
+                            countries: [elements[i].getElementsByTagName("CtryNm")[0].innerHTML],
+                        };
+                        currencies.push(currency);
+                    }
+                }
+            } 
+            this.props.whenDone(currencies);
+        })
+    }  
+
+    render() {
+        return null;
+    }
+}
+
+export default CurrencyScraper; 
