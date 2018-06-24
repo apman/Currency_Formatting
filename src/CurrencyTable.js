1ec645522ef93056979a1a80bc0371fbe6c327e3
apman <anna.malchowperryman@shopify.com>
2018-06-23 22:09:08 -0400

    Change table to plain html

--- src/CurrencyTable.js
+++ src/CurrencyTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Card, DataTable, Link, Tooltip} from '@shopify/polaris';
+import {Card, Link, Tooltip} from '@shopify/polaris';
 import autoBind from 'react-autobind';
 
 
@@ -22,34 +22,36 @@ class CurrencyTable extends Component {
 
   currencyRow(currency) {
     const {locales} = this.props;
-    const rowHeading = <Tooltip content={JSON.stringify(currency.countries, null, 1)}><Link>{currency.code}<br />{currency.name}</Link></Tooltip>
-    const formattedValues = locales.map(locale => new Intl.NumberFormat(locale, {style: 'currency', currency: currency.code}).format(9.99));
-    return [rowHeading].concat(formattedValues);
+    const rowHeading = <th><Tooltip content={JSON.stringify(currency.countries, null, 1)}><Link>{currency.code}<br />{currency.name}</Link></Tooltip></th>
+    const formattedValues = locales.map(locale => <td>{new Intl.NumberFormat(locale, {style: 'currency', currency: currency.code}).format(9.99)}</td>);
+    return <tr>{rowHeading}{formattedValues}</tr>;
     //  ???  would it be preferable to use a for loop to push individual formatted values onto [rowHeading] to avoid
     //        the reshuffle ?
   }
 
   localeHeadings() {
     const {locales} = this.props;
-    return ['Currency'].concat(locales);
+    const headings = locales.map(locale => <th>{locale}</th>);
+    return headings;
   }
+
+  // const test = 'hello';
   
   render() {
     const {currencies, locales} = this.props;
-      const rows = this.currencyRows(); 
-    const columnContentTypes = ['text'].concat(Array(this.state.showColumns.length).fill('numeric'));
-
+    const rows = this.currencyRows(); 
     const headings = this.localeHeadings();
 
     return (
-      <Card>
-        <DataTable
-          columnContentTypes={columnContentTypes}
-          headings={headings}
-          rows={rows}
-          footerContent={`Showing ${rows.length} of ${currencies.length} currencies, and ${headings.length - 1} of ${locales.length} locales`}
-        />
-      </Card>
+      <div>
+        <table>
+          <thead>
+            <tr><th>Currency</th>{headings}</tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+        <div>{`Showing ${rows.length} of ${currencies.length} currencies, and ${headings.length - 1} of ${locales.length} locales`}</div>
+      </div>
     );
   }
 
